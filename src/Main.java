91eb49c4c36849dc132a3318036194d68201d68a
Kihtenko Olga <kiok.ko1811@gmail.com>
2024-08-08 13:00:45 +0300

    (feat) developing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         User ivan = new User("Ivan", 22);
-        System.out.println("Hello, " +ivan.getName()+ "!");
+        System.out.println("Hello, " +ivan.getName()+ ", "+ivan.getAge()+" y.o.!");
     }
 }
