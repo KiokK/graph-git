b64587cf5b70652807b0a33d11f122cdcc0cf096
Kihtenko Olga <kiok.ko1811@gmail.com>
2024-08-08 20:00:32 +0300

    (fix) fix release

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         User ivan = new User("Ivan", 22);
         System.out.println("Hello, " +ivan.getName()+ ", "+ivan.getAge()+" y.o.");
-        Car car = new Car(430, "BMV");
+        Car car = new Car(430, "BMV X7");
         System.out.println("Car: " + car.getModel() + ", speed: " + car.getSpeed() + " (km/h)");
     }
 }
